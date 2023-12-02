ce15bad66cad6653e92eb341b8737aa4a8687e81
Anton Tuzhik <TuzhikAA@gmail.com>
2023-12-02 23:50:58 +0100

    day 2 part 2

--- day2/bin/main.ml
+++ day2/bin/main.ml
@@ -1,20 +1,22 @@
-let max_red = 12
-let max_green = 13
-let max_blue = 14
+type cubes = { red : int; green : int; blue : int }
 
-let is_possible_game g =
-  if
-    g.Lib.Helpers.subsets
-    |> List.for_all (fun subset ->
-           subset
-           |> List.for_all (fun cube ->
-                  match cube with
-                  | Lib.Helpers.Red count when count <= max_red -> true
-                  | Green count when count <= max_green -> true
-                  | Blue count when count <= max_blue -> true
-                  | _ -> false))
-  then Some g.game_id
-  else None
+let get_fewer_numbers_cubes_for_game g =
+  g.Lib.Helpers.subsets
+  |> List.fold_left
+       (fun acc subset ->
+         subset
+         |> List.fold_left
+              (fun (acc : cubes) (cube : Lib.Helpers.cube) ->
+                match cube with
+                | Lib.Helpers.Red count ->
+                    if count > acc.red then { acc with red = count } else acc
+                | Green count ->
+                    if count > acc.green then { acc with green = count }
+                    else acc
+                | Blue count ->
+                    if count > acc.blue then { acc with blue = count } else acc)
+              acc)
+       { red = 0; green = 0; blue = 0 }
 
 let game_sum =
   Iter.IO.lines_of "input.txt"
@@ -24,10 +26,8 @@ let game_sum =
          with
          | Ok v -> v
          | Error msg -> failwith msg)
-  |> Iter.map (fun game_line ->
-         match is_possible_game game_line with
-         | None -> 0
-         | Some game_id -> game_id)
+  |> Iter.map get_fewer_numbers_cubes_for_game
+  |> Iter.map (fun { red; green; blue } -> red * green * blue)
   |> Iter.sum
 ;;
 
